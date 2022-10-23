2b53a0721963d3329b12dd5a14ffbcc994371eb1
apps-mahmudbakale <apps.mahmud.bakale@gmail.com>
2022-10-23 16:00:38 +0100

    WIp

---
+++ resources/views/layouts/welcome.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>SMMIQGSSOSA | Official Home Page</title>
+
+    <meta name="description" content="Old Boys Association of Sultan Muhammad Maccido Institute for Qur'an and General Studies Sokoto official Website" />
+    <meta name="keywords" content="Institute Sokoto, Sultan Muhammad Maccido" />
+    <meta name="author" content="Mahmud Bakale" />
+
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+
+    <!-- GOOGLE FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet" />
+    <!-- BOOTSTRAP CSS -->
+    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/vendor/navbar/bootstrap-4-navbar.css" />
+
+    <!--Animate css -->
+    <link rel="stylesheet" href="assets/vendor/animate/animate.css" media="all" />
+
+    <!-- FONT AWESOME CSS -->
+    <link rel="stylesheet" href="assets/vendor/fontawesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <!--owl carousel css -->
+    <link rel="stylesheet" href="assets/vendor/owl-carousel/owl.carousel.css" media="all" />
+
+    <!--Magnific Popup css -->
+    <link rel="stylesheet" href="assets/vendor/magnific/magnific-popup.css" media="all" />
+
+    <!--Nice Select css -->
+    <link rel="stylesheet" href="assets/vendor/nice-select/nice-select.css" media="all" />
+
+    <!--Offcanvas css -->
+    <link rel="stylesheet" href="assets/vendor/js-offcanvas/css/js-offcanvas.css" media="all" />
+
+    <!-- MODERNIZER  -->
+    <script src="assets/vendor/modernizr/modernizr-custom.js"></script>
+
+    <!-- Main Master Style  CSS  -->
+    <link id="cbx-style" data-layout="1" rel="stylesheet" href="assets/css/style-default.min.css" media="all" />
+</head>
+
+<body>
+    @include('partials.header')
+
+    @yield('content')
+
+    @include('partials.footer')
+    <script src="assets/vendor/jquery/jquery-3.3.1.min.js"></script>
+
+    <!-- POPPER JS -->
+    <script src="assets/vendor/bootstrap/js/popper.min.js"></script>
+
+    <!-- BOOTSTRAP JS -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/vendor/navbar/bootstrap-4-navbar.js"></script>
+
+    <!--owl-->
+    <script src="assets/vendor/owl-carousel/owl.carousel.min.js"></script>
+
+    <!--Waypoint-->
+    <script src="assets/vendor/waypoint/waypoints.min.js"></script>
+
+    <!--CounterUp-->
+    <script src="assets/vendor/counterup/jquery.counterup.min.js"></script>
+
+    <!--isotope-->
+    <script src="assets/vendor/isotope/isotope.pkgd.min.js"></script>
+
+    <!--magnific-->
+    <script src="assets/vendor/magnific/jquery.magnific-popup.min.js"></script>
+
+    <!--Smooth Scroll-->
+    <script src="assets/vendor/smooth-scroll/jquery.smooth-scroll.min.js"></script>
+
+    <!--Jquery Easing-->
+    <script src="assets/vendor/jquery-easing/jquery.easing.1.3.min.js"></script>
+
+    <!--Nice Select -->
+    <script src="assets/vendor/nice-select/jquery.nice-select.js"></script>
+
+    <!--Jquery Valadation -->
+    <script src="assets/vendor/validation/jquery.validate.min.js"></script>
+    <script src="assets/vendor/validation/additional-methods.min.js"></script>
+
+    <!--off-canvas js -->
+    <script src="assets/vendor/js-offcanvas/js/js-offcanvas.pkgd.min.js"></script>
+
+    <!-- Countdown -->
+    <script src="assets/vendor/jquery.countdown/jquery.countdown.min.js"></script>
+
+    <!-- custom js: main custom theme js file  -->
+    <script src="assets/js/theme.min.js"></script>
+
+    <!-- slick carousel slider -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script><!-- custom js: custom js file is added for easy custom js code  -->
+    <script src="assets/js/custom.js"></script>
+</body>
+
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,85 @@
+<!--== Header Area Start ==-->
+<header id="header-area">
+    <div class="preheader-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-7 col-7">
+                    <div class="preheader-left">
+                        <a href="mailto: info@nacos.org.ng"><strong>Email:</strong> info@nacos.org.ng</a>
+                        <a href="tel:+234 809 246 1400"><strong>Hotline:</strong> +234 809 246 1400</a>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-sm-5 col-5 text-right">
+                    <div class="preheader-right">
+                        <!-- <a title="Login" class="btn-auth btn-auth-rev" href="register.php">Login</a> -->
+                        <a title="Register" class="btn-auth btn-auth" href="register.html"><i
+                                class="fa fa-graduation-cap"></i> Member Portal</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="header-bottom-area" id="fixheader">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <nav class="main-menu navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="index.html">
+                            <img src="{{ asset('logo.png') }}" alt="Logo" />
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#menucontent" aria-controls="menucontent" aria-expanded="false">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="menucontent">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="about.html" data-toggle="dropdown"
+                                        role="button">About</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item "><a class="nav-link"
+                                                href="about.html">Association</a></li>
+                                        <li class="nav-item "><a class="nav-link" href="executive.html">National
+                                                Executive</a></li>
+                                        <li class="nav-item "><a class="nav-link" href="boards.html">National
+                                                Advisory Board</a></li>
+                                        <li class="nav-item "><a class="nav-link" href="pastPreidents.html">Past
+                                                National President</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item "><a class="nav-link" href="event.html">Event</a></li>
+                                <li class="nav-item "><a class="nav-link" href="gallery.html">Gallery</a></li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="member.html" data-toggle="dropdown"
+                                        role="button">Links</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item "><a class="nav-link" href="member.html">Members
+                                                Directory</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="chapter.html">New Chapter
+                                                Registration</a></li>
+                                        <li class="nav-item"><a class="nav-link"
+                                                href="eligiblechapters.html">Eligigble Chapters </a></li>
+                                        <li class="nav-item"><a class="nav-link" href="Annual_Dues.html">
+                                                Chapters' Annual Dues</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="boards.html">Member
+                                                Welfare Scheme</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="updates.html">NACOS
+                                                Updates</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item "><a class="nav-link" href="register.html">Accounts</a></li>
+                                <li class="nav-item "><a class="nav-link" href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!--== Header Area End ==-->
+<!--== Header Area End ==-->
